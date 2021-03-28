6e383ad06300b4d1162d03c5be968df8fdfba5cf
Jose Vicente <jantvb@gmail.com>
2021-03-28 18:16:52 -0400

    New components

--- src/app/route/admin/admin-routing.module.ts
+++ src/app/route/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
+import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  { path: '', component: AdminComponent },
+  { path: 'user', component: UserComponent }
 ];
 
 @NgModule({
--- src/app/route/admin/admin.module.ts
+++ src/app/route/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ApartmentComponent } from './apartment/apartment.component';
+import { BuildingComponent } from './building/building.component';
+import { DocumentComponent } from './document/document.component';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ApartmentComponent, BuildingComponent, DocumentComponent, UserComponent, RoleComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
