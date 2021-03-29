a5200d5af4979833897ac59953fb1a7296e8af43
Joaquin Navarro <alfian1991@gmail.com>
2021-03-28 21:03:19 -0400

    adding sideNav

--- src/app/route/admin/admin-routing.module.ts
+++ src/app/route/admin/admin-routing.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'role', loadChildren: () => import('./role/role.module').then(m => m.RoleModule) },
-  { path: 'document', loadChildren: () => import('./document/document.module').then(m => m.DocumentModule) },
-  { path: 'apartment', loadChildren: () => import('./apartment/apartment.module').then(m => m.ApartmentModule) },
-  { path: 'building', loadChildren: () => import('./building/building.module').then(m => m.BuildingModule) }
+  { path: '', component: AdminComponent, children: [
+    { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+    { path: 'role', loadChildren: () => import('./role/role.module').then(m => m.RoleModule) },
+    { path: 'document', loadChildren: () => import('./document/document.module').then(m => m.DocumentModule) },
+    { path: 'apartment', loadChildren: () => import('./apartment/apartment.module').then(m => m.ApartmentModule) },
+    { path: 'building', loadChildren: () => import('./building/building.module').then(m => m.BuildingModule) }
+  ]}
 ];
 
 @NgModule({
--- src/app/route/admin/admin.module.ts
+++ src/app/route/admin/admin.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { AdminComponent } from './admin.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent
+  ],
   imports: [
     CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
     AdminRoutingModule
   ]
 })
