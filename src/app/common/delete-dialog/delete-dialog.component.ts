84e6b9522262a9ec1b3696d649d11f9fd53b61e4
Joaquin Navarro <alfian1991@gmail.com>
2021-04-10 12:36:26 -0400

    removed unused dependencies in deleteDialog

--- src/app/common/delete-dialog/delete-dialog.component.ts
+++ src/app/common/delete-dialog/delete-dialog.component.ts
@@ -1,10 +1,5 @@
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, OnInit, Inject } from '@angular/core';
-import { Role } from 'src/app/model/role.model';
-import { User } from 'src/app/model/user.model';
-import { Apartment } from 'src/app/model/apartment.model';
-import { Building } from 'src/app/model/building.model';
-import { Document } from 'src/app/model/document.model';
 
 @Component({
   selector: 'app-delete-dialog',
