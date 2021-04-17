fbdfb80cf8e558405733e13bf00f5c9e1405cb7a
Joaquin Navarro <alfian1991@gmail.com>
2021-04-17 18:24:22 -0400

    removing

--- src/app/service/authService/request.interceptor.service.ts
+++ src/app/service/authService/request.interceptor.service.ts
@@ -1,9 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { SessionService } from './session.service';
 
