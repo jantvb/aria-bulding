eedf30928830d3ac8ffb57c914cdc18b9247033a
Joaquin Navarro <alfian1991@gmail.com>
2021-04-17 18:38:03 -0400

    adding response.Interceptor logic

--- src/app/service/authService/response.interceptor.service.ts
+++ src/app/service/authService/response.interceptor.service.ts
@@ -1,16 +1,41 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResponseInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService,
+              private snackBar: MatSnackBar,
+              private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+
+    return next.handle(req)
+               .pipe(
+                  tap(),
+                  catchError((error: HttpErrorResponse) => {
+
+                    if (error.status === 401 || 403) {
+                      this.authService.logout();
+                    }
+
+                    this.snackBar
+                          .open(error.message,
+                                'Dismiss',
+                                {duration: 3500});
+
+                    return throwError(error);
+                  })
+               )
+
   }
 
 
